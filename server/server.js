070996beda70b533b6f4c95360e8448e7e9881bb
Harsha-cpu-prog <harshanenwani121@gmail.com>
2025-04-30 17:31:42 +0530

    changes made

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 // CORS
 app.use(
   cors({
-    origin: "http://localhost:1573",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
